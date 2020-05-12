466021fe4c8f41b638f3c13be71af2a2d1790f02
Marcos Paulo Nascimento <marcos.paulo.nascimento1@gmail.com>
2020-05-12 00:14:01 -0300

    Update publicacao_model.php
    commit

--- model/publicacao_model.php
+++ model/publicacao_model.php
@@ -1,5 +1,28 @@
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	datapublicacao DATE,
-	conteudo VARCHAR(200),
-	userid INT NOT NULL,
-	FOREIGN KEY (userid) REFERENCES usuario(id) ON DELETE CASCADE
+<?php
+    class Publicacao
+    {
+        static function Cadastro($datapublicacao, $conteudo, $userid)
+        {     
+            $objDb = new db();
+            $link = $objDb->mysqlConnect();
+    
+            $stmt = $link->prepare("INSERT INTO publicacao(datapublicacao, conteudo, userid) VALUES (?, ?, ?);");
+            $stmt->bind_param("sss", $datapublicacao, $conteudo, $userid);
+            $runquery = $stmt->execute();
+    
+            if($runquery)
+            {
+                $id = $link->insert_id;
+                $stmt2 = $link->prepare("INSERT INTO contato(telefone, email, endereco, userid) VALUES (?, ?, ?, ?);");
+                $stmt2->bind_param("ssss", $phone, $email, $address, $id);
+                $runquery2 = $stmt2->execute();
+                if($runquery2)
+                    return "Sucesso ao cadastrar usuário!";
+                else
+                    return "Falha ao cadastrar usuário: ".mysqli_error($link);
+            }
+            else
+                return "Falha ao cadastrar usuário: ".mysqli_error($link);
+        }
+    }
+?>
